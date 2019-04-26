0ad6842e5d8c06446c662102a49e19564c2dda39
Natalia Zhornitskaya <n.zhornitskaya@wis.eu.com>
2019-04-26 18:02:25 +0300

    add component export to public_api

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,9 +1,12 @@
 /*
- * Public API Surface of fs-menu
+ * Public API Surface of ngx-autocomplete
  */
 
 // Modules
 export { FsAutocompleteModule } from './app/fs-autocomplete.module';
+
+// Components
+export { FsAutocompleteComponent } from './app/components/autocomplete/autocomplete.component';
 
 // Services
 // export { FsMessage } from './app/message.service';
