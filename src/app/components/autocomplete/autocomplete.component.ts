031fd021fda533e639ef5c498e9eec990836ff85
Anatoly <mendor1990@gmail.com>
2021-10-04 13:59:16 +0300

    added appearance

--- src/app/components/autocomplete/autocomplete.component.ts
+++ src/app/components/autocomplete/autocomplete.component.ts
@@ -84,6 +84,7 @@ export class FsAutocompleteComponent implements ControlValueAccessor, OnInit, On
   @Input() public required = false;
   @Input() public disabled = false;
   @Input() public hint: string = null;
+  @Input() public appearance: string = 'standard';
 
   @Input('panelClass')
   set setPanelClass(value) {
