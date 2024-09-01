fcbf121607ce3233d8fccc0a9a2bd6a6ef82a2b6
Eugene Kubesh <codbasters@gmail.com>
2024-09-01 18:14:52 +0200

    Revert "DT-T2836 call onChange for static select"
    This reverts commit fad7ad4aff4ec268f6527ccd9837bec8b133facf.

--- src/app/components/autocomplete/autocomplete.component.ts
+++ src/app/components/autocomplete/autocomplete.component.ts
@@ -292,7 +292,6 @@ export class FsAutocompleteComponent implements ControlValueAccessor, OnInit, On
   public select(value) {
     if (this._isStaticSelected(value)) {
       this.staticSelect(value.staticOptionIndex);
-      this._onChange(value);
     } else {
       this.model = value;
       this.data = [];
